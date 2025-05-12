f11ab873af6f765b4750e69fae71b12a35e1be28
Han Sol Jin <hansol@hansol.ca>
2025-05-12 02:25:37 -0700

    sofia: add audio configs
    sofia_retail-user 11 RPMS31.Q1-54-13-11 e171d1 release-keys
    
    Change-Id: I5e2fc07f131ea0556fd3091d0b48850f083a58fb

--- device.mk
+++ device.mk
@@ -8,5 +8,9 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # Inherit from common
 $(call inherit-product, device/motorola/sm6125-common/sm6125-common.mk)
 
+# Audio
+$(foreach f,$(wildcard $(LOCAL_PATH)/audio/*),\
+        $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/etc/$(notdir $f)))
+
 # Inherit from vendor blobs
 $(call inherit-product, vendor/motorola/sofia/sofia-vendor.mk)
